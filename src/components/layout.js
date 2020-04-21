67b07b835578cba2e7e9cf5280ed23904337388c
EC2 Default User <ec2-user@ip-172-31-36-78.us-east-2.compute.internal>
2020-04-21 19:37:46 +0000

    g

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,42 +1,57 @@
 import React  from 'react'
 import Navbar from "./Navabar/Navbar.js"
+  import {Wrapper} from "./Shared.js"
  import Parallax from 'react-springy-parallax'
  import Animated from 'animated/lib/targets/react-dom'
  import Easing from 'animated/lib/Easing'
 
+
+
 import "./layout.css"
-import bgSVG from "./../images/bg.svg"
+import campBG from "./../images/campBG.svg"
+import campMBG from "./../images/campMBG.svg"
+import campMG from "./../images/campMG.svg"
+import campMFG from "./../images/campMFG.svg"
+import campFG from "./../images/campFG.svg"
+
+
+
+const BgLayer = (props) => {
+  const {   image, offset, speed} = props;
+
+  return (
+    <div className="outerDiv"  >
+    
+   <Parallax.Layer offset= {offset}  speed={speed}  className="ParallaxDiv" >
+                       <img src={image} style={{ minHeight: "200vh",  minWidth:"100vw"}} className="ImageBG"/> 
+                     </Parallax.Layer>
+      
+    </div>
+  )
+}
+
+
+
 const Layout = ({children}) => {
    
          
         return (
-            <body style={{overflow: "hidden"}} >
+            <body   >
                  <Navbar style={{position:"sticky", top: "0"}} />
-           
-           
-                 <Parallax  pages={4}   
-    effect={(animation, toValue) =>
-    Animated.timing(animation, { toValue, duration: 0 })}
-        >
-          
-                    <Parallax.Layer
-                            tension={0}
-                            offset={0}
-                            speed={-0.7}
-                           style={{ position: 'absolute' }}
-                            >
-                   
-                         <img src={bgSVG} style={{ maxHeight: "200vh", winWidth:"100vw"}}/>
-                   
-                     </Parallax.Layer>
-                     
-                     
-            <Parallax.Layer
-                    offset={0}
-                    speed={0.1}
-                    >
-                   {children}
-                </Parallax.Layer>
+                 <Parallax  pages={4}    effect={(animation, toValue) =>  Animated.timing(animation, { toValue, duration: 0 })}  >
+                 
+                    <BgLayer   image={campBG} offset = " 0" speed="-0.75"/>
+                    <BgLayer   image={campMBG} offset = " 0" speed="-0.7"/>
+                    <BgLayer   image={campMG} offset = " 0" speed="-0.68"/>
+                    <BgLayer   image={campMFG} offset = " 0" speed="-0.65"/>
+                    <BgLayer   image={campFG} offset = " 0.1" speed="-0.6"/>
+                    
+                     <Parallax.Layer
+                        offset={0}
+                        speed={0.1}
+                        >
+                       {children}
+                    </Parallax.Layer>
                </Parallax>
              
             
